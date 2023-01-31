5ffbabfe09a60ae4da59d8fe59bdfa747fc18822
Gustavo Lira <gu.lirar23@gmail.com>
2023-01-31 13:39:19 -0600

    test files

--- todo-server/src/main/java/com/thdtraining/todoserver/security/UserDetailsImp.java
+++ todo-server/src/main/java/com/thdtraining/todoserver/security/UserDetailsImp.java
@@ -2,7 +2,7 @@ package com.thdtraining.todoserver.security;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
+//import java.util.Collections;
 import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
