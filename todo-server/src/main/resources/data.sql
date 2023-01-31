9fe795cf4dd4d71a6ec8fd6dd25f65e0aead911b
Gustavo Lira <gu.lirar23@gmail.com>
2023-01-31 13:39:02 -0600

    add: users are loaded to the DB when the server app run

---
+++ todo-server/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO user_table (EMAIL, FIRST_NAME, LAST_NAME, USER_PASSWORD, USER_ROLE) VALUES ('gustavo@email.com','Gustavo','Lira','$2a$12$lLDG6ir1eRwdxLwR47YxPOdPHSsmQFIQdWujNnsmHd3/4l48JcE5e','ADMIN'); /* 1234gus */
+INSERT INTO user_table (EMAIL, FIRST_NAME, LAST_NAME, USER_PASSWORD, USER_ROLE) VALUES ('adan@email.com','Adan','Alvarez','$2a$12$5t9x57mV.J4HskJnky5UBuwjZFTaG4YmA/pnLvuD9/xM.WFmc.Yta','ADMIN'); /* 1234adan */
+INSERT INTO user_table (EMAIL, FIRST_NAME, LAST_NAME, USER_PASSWORD, USER_ROLE) VALUES ('andres@email.com','Andres','Avalos','$2a$12$DWt.gkXe8FgFUUlFg.wZ8ue.4g57FtN5LO/D6J.FukX9LFZs2Alze','ADMIN'); /* 1234andres */
+INSERT INTO user_table (EMAIL, FIRST_NAME, LAST_NAME, USER_PASSWORD, USER_ROLE) VALUES ('bryan@email.com','Bryan','Gongora','$2a$12$pcf9p9nhZ/cbuSswlOb36ecABgyi50U1OUBXbf26gtQrhJyPCGNGe','REGULAR'); /*  1234bryan */
+INSERT INTO user_table (EMAIL, FIRST_NAME, LAST_NAME, USER_PASSWORD, USER_ROLE) VALUES ('ricardo@email.com','Ricardo','Zapata','$2a$12$sM3288ZRJoKWNOqpjxxt7uU86K9J8UmukaGaRndTukMm7rDy18bRa','REGULAR'); /* 1234ricardo */
