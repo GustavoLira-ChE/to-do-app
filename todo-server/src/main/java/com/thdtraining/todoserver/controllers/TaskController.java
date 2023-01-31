94eef1042ec2a680fc3708dfc17f15aaea4a7055
Gustavo Lira <gu.lirar23@gmail.com>
2023-01-31 13:42:44 -0600

    add cross origin annotation

--- todo-server/src/main/java/com/thdtraining/todoserver/controllers/TaskController.java
+++ todo-server/src/main/java/com/thdtraining/todoserver/controllers/TaskController.java
@@ -3,6 +3,7 @@ package com.thdtraining.todoserver.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.thdtraining.todoserver.services.TaskService;
 
 @RestController
 @RequestMapping("/task")
+@CrossOrigin("http://localhost:4200")
 public class TaskController {
     
     private TaskService taskService;
