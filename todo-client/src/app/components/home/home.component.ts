b31c003c757df281c0b42521540d14b479c81ee1
Gustavo Lira <gu.lirar23@gmail.com>
2023-02-08 12:16:03 -0600

    create a home page component

---
+++ todo-client/src/app/components/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent{
+
+}
